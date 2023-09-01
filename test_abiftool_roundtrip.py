be90f6112d4d63d5332e6a38fa5034b99a1ea18d
Rob Lanphier <robla@robla.net>
2023-08-31 18:19:45 -0700

    Fixed up roundtrip test, and added a new one.

--- test_abiftool_roundtrip.py
+++ test_abiftool_roundtrip.py
@@ -1,16 +1,32 @@
 import subprocess
 import os
 import glob
+import sys
 
 def test_roundtrip_conversion():
     abif_files = [
-        "testdata/electorama-abif/testfiles/potus1980test01.abif"
+        ("testdata/electorama-abif/testfiles/potus1980test01.abif",
+         "20010:Carter>Anderson>Reagan"),
+        ("testdata/electorama-abif/testfiles/test001.abif",
+         "7:Georgie/5>Allie/4>Dennis/3=Harold/3>Candace/2>Edith/1>Billy/0=Frank/0")
         ]
-    # next one to try: "testdata/abif_testsuite/test001.abif"
 
-    for abif_file in abif_files:
+    for (fn, abstr) in abif_files:
+        try:
+            fh = open(fn, 'rb')
+        except:
+            print(f'Missing file: {fn}')
+            print(
+                "Please run './repomgr.py *.repospec.json' " +
+                "if you haven't already")
+            sys.exit()
+
+    ##########################
+    for (fn, abstr) in abif_files:
         # Convert abif to jabmod
-        jabmod_content = subprocess.run(["abiftool.py", "-t", "jabmod", abif_file], capture_output=True, text=True).stdout
+        jabmod_content = subprocess.run(["abiftool.py", "-t", "jabmod", fn],
+                                        capture_output=True,
+                                        text=True).stdout
 
         # Convert jabmod back to abif after roundtrip
         roundtrip_abif_content = subprocess.run(["abiftool.py",
@@ -18,5 +34,5 @@ def test_roundtrip_conversion():
                                                  "-t", "abif", "-"
                                                  ], capture_output=True,
                                                 text=True, input=jabmod_content).stdout
-        assert "20010:Carter>Anderson>Reagan" in roundtrip_abif_content
+        assert abstr in roundtrip_abif_content
 
