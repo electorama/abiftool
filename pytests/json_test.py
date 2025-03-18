af2950a3c77c19b09a53faebcedbb7ef9bae18c5
Rob Lanphier <robla@robla.net>
2025-03-18 00:04:38 -0700

    Adding tests for the old json and new json command lines
    Testing the following parameters and combos:
    * -t irvjson
    * -t paircountjson
    * -t json -m IRV
    * -t json -m pairwise

--- pytests/json_test.py
+++ pytests/json_test.py
@@ -9,6 +9,8 @@ import pytest
 
 
 testlist = [
+    # TEST 001:
+    # FIXME: add test id and description of test
     (
         ['-f', 'abif', '-t', 'winlosstiejson'],
         'testdata/tenn-example/tennessee-example-simple.abif',
@@ -16,6 +18,8 @@ testlist = [
         ["Chat", "wins"],
         2
     ),
+    # TEST 002:
+    # FIXME: add test id and description of test
     (
         ['-f', 'abif', '-t', 'jabmod'],
         'testdata/tenn-example/tennessee-example-simple.abif',
@@ -23,6 +27,8 @@ testlist = [
         ["votelines", 0, "qty"],
         42
     ),
+    # TEST 003:
+    # FIXME: add test id and description of test
     (
         ['-f', 'jabmod', '-t', 'jabmod'],
         'testdata/california/simple001-example.jabmod.json',
@@ -30,6 +36,8 @@ testlist = [
         ["votelines", 0, "qty"],
         1
     ),
+    # TEST 004:
+    # FIXME: add test id and description of test
     (
         ['-f', 'jabmod', '-t', 'jabmod', '-m', 'consolidate'],
         'testdata/california/simple001-example.jabmod.json',
@@ -37,6 +45,8 @@ testlist = [
         ["votelines", 0, "qty"],
         5
     ),
+    # TEST 005:
+    # FIXME: add test id and description of test
     (
         ['-f', 'abif', '-t', 'irvjson'],
         'testdata/california/sf2018special-results.abif',
@@ -44,6 +54,8 @@ testlist = [
         ["roundmeta", -1, "winner"],
         ["LONDON_BREED"]
     ),
+    # TEST 006:
+    # FIXME: add test id and description of test
     (
         ['-f', 'abif', '-t', 'irvjson'],
         'testdata/california/sf2018special-results.abif',
@@ -51,6 +63,8 @@ testlist = [
         ["roundmeta", -1, "eliminated"],
         ["MARK_LENO"]
     ),
+    # TEST 007:
+    # FIXME: add test id and description of test
     (
         ['-f', 'abif', '-t', 'irvjson'],
         'testdata/california/sf2018special-results.abif',
@@ -58,6 +72,8 @@ testlist = [
         ["roundmeta", -1, "startingqty"],
         254016
     ),
+    # TEST 008:
+    # FIXME: add test id and description of test
     # FIXME - the report from the city says Breed won with 115977, but my
     # count shows 116020
     # 
@@ -70,6 +86,8 @@ testlist = [
         ["rounds", -1, "LONDON_BREED"],
         116020
     ),
+    # TEST 009:
+    # FIXME: add test id and description of test
     (
         (['-f', 'abif', '-t', 'irvjson'],
          'testdata/california/sf2018special-results.abif',
@@ -77,6 +95,8 @@ testlist = [
          ["roundmeta", 0, "eliminated", 3],
          r'WRITE_IN')
     ),
+    # TEST 010:
+    # FIXME: add test id and description of test
     (
         (['-f', 'abif', '-t', 'irvjson'],
          'testdata/mock-elections/mock-twotie.abif',
@@ -84,6 +104,8 @@ testlist = [
          ["roundmeta", 13, "all_eliminated"],
          r'F')
     ),
+    # TEST 011:
+    # FIXME: add test id and description of test
     (
         (['-f', 'abif', '-t', 'jabmod'],
          'localabif/bolson-nameq/votedata-2024-01-27/2022-08-16_Alaska-U.S._Representative_(Special_General).abif',
@@ -91,6 +113,8 @@ testlist = [
          ["candidates"],
          4)
     ),
+    # TEST 012:
+    # FIXME: add test id and description of test
     (
         (['-f', 'abif', '-t', 'paircountjson'],
          'testdata/commasep/commasquare.abif',
@@ -98,6 +122,8 @@ testlist = [
          ["A,X", "B,Y"],
          12)
     ),
+    # TEST 013:
+    # FIXME: add test id and description of test
     (
         (['-f', 'abif', '-t', 'jabmod'],
          'testdata/commasep/commasquare.abif',
@@ -105,6 +131,40 @@ testlist = [
          ["votelines", 0, "prefs", "C,Z", "rank"],
          3)
     ),
+    # TEST 014:
+    # Test the deprecated '-t paircountjson' parameter, which will be
+    # replaced by the "-t json -m pairwise" combo
+    pytest.param(['-f', 'abif', '-t', 'paircountjson'],
+                 'testdata/mock-elections/tennessee-example-simple.abif',
+                 'is_equal',
+                 ["Chat", "Knox"],
+                 83,
+                 id='test014'),
+    # TEST 015:
+    # Test the "-t json -m pairwise" combo
+    pytest.param(['-f', 'abif', '-t', 'json', '-m', 'pairwise'],
+                 'testdata/mock-elections/tennessee-example-simple.abif',
+                 'is_equal',
+                 ["Chat", "Knox"],
+                 83,
+                 id='test015'),
+    # TEST 016:
+    # Test the deprecated '-t irvjson' parameter, which will be
+    # replaced by "-t json -m IRV" combo
+    pytest.param(['-f', 'abif', '-t', 'irvjson'],
+                 'testdata/mock-elections/tennessee-example-simple.abif',
+                 'is_equal',
+                 ["winner", 0],
+                 "Knox",
+                 id='test016'),
+    # TEST 017:
+    # Test the "-t json -m IRV" combo
+    pytest.param(['-f', 'abif', '-t', 'json', '-m', 'IRV'],
+                 'testdata/mock-elections/tennessee-example-simple.abif',
+                 'is_equal',
+                 ["winner", 0],
+                 "Knox",
+                 id='test017'),
 ]
 
 @pytest.mark.parametrize(
