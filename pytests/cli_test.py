electorama/abiftool

commit 25083b4efc3b693f1ff10e0cb9e840432349d2f8

Adding support for SF's json format read from zipfile
I had pretty good luck with Google Gemini reading my code and figuring
out how to graft in some standalone scripts to make it so that we can
now count the SF mayoral election of 2024 (and maybe other races).

abiftool.py
@@ -20,6 +20,7 @@ import sys
 
 try:
     from abiflib import *
+    from abiflib.sfjson_fmt import convert_sfjson_to_jabmod, list_contests
 except ModuleNotFoundError as e:
     print(f"ModuleNotFoundError: {e.name}\n")
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
@@ -41,6 +42,7 @@ INPUT_FORMATS = [
     {'jabmod': 'Internal JSON ABIF model (Json ABIF MODel)'},
     {'nameq': 'Brian Olson\'s format which URL-encoded version of the raw ballots'},
     {'preflib': 'Files downloaded from preflib.org'},
+    {'sfjson': 'San Francisco JSON CVR format'},
     {'sftxt': 'Text files published by the City and County of San Francisco'},
     {'widj': 'Legacy format from Electowidget'}
 ]
@@ -113,7 +115,9 @@ def main():
     validinfmts = get_keys_from_dict_list(INPUT_FORMATS)
     validoutfmts = get_keys_from_dict_list(OUTPUT_FORMATS)
     validmod = get_keys_from_dict_list(MODIFIERS)
-    parser.add_argument('input_file', nargs='+',
+    parser.add_argument('--container',
+                        help='Container file (e.g., zip, tar.gz)')
+    parser.add_argument('input_file', nargs='*',
                         help='Input file(s) to convert (--help for list of options)')
     parser.add_argument('-d', '--debug', action="store_true",
                         help='Output debug information if available')
@@ -129,17 +133,29 @@ def main():
                         help='Clean whitespace in ABIF file')
     parser.add_argument('--add-scores', action="store_true",
                         help='Add scores to votelines when only rankings are provided')
+    parser.add_argument('--contestid', type=int,
+                        help='The ID of the contest to process from a container')
+    parser.add_argument('-l', '--list-contests', action='store_true',
+                        help='List contests in a container and exit')
 
     args = parser.parse_args()
     abiflib_test_log(f"cmd: {' '.join(sys.argv)}")
+
+    if args.list_contests:
+        if args.container:
+            list_contests(args.container)
+            sys.exit()
+        else:
+            print("Error: The --list-contests flag requires a container specified with --container.")
+            sys.exit()
 
     # Determine input format based on file extension or override from
     # the "-f/--fromfmt" option
     if args.fromfmt:
         input_format = args.fromfmt
-    elif args.input_file == '-':
+    elif args.input_file and args.input_file[0] == '-':
         parser.error("The -f parameter is required with '-'")
-    elif args.input_file[0].find('.') >= 0:
+    elif args.input_file and args.input_file[0].find('.') >= 0:
         _, file_extension = args.input_file[0].rsplit('.', 1)
         input_format = file_extension
     else:
@@ -173,7 +189,21 @@ def main():
     add_ratings = args.add_scores
 
     storecomments = 'jcomments' in modifiers
-    if (input_format == 'abif'):
+    if 'list-contests' in modifiers:
+        if args.container:
+            list_contests(args.container)
+            sys.exit()
+        else:
+            print("Error: The --list-contests modifier requires a container specified with --container.")
+            sys.exit()
+
+    if args.container:
+        if input_format == 'sfjson':
+            abifmodel = convert_sfjson_to_jabmod(args.container, contestid=args.contestid)
+        else:
+            print(f"Error: The --container flag is not supported for the '{input_format}' format yet.")
+            sys.exit()
+    elif (input_format == 'abif'):
         try:
             abifmodel = convert_abif_to_jabmod(inputstr,
                                                cleanws=args.cleanws,

abiflib/sfjson_fmt.py
@@ -1,0 +1,122 @@
+#!/usr/bin/env python3
+'''abiflib/sfjson_fmt.py - San Francisco JSON CVR format support'''
+
+# Copyright (c) 2025 Rob Lanphier
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import json
+import re
+import zipfile
+from abiflib.core import get_emptyish_abifmodel
+
+def _short_token(longstring, max_length=20, add_sha1=False):
+    if len(longstring) <= max_length and \
+       re.match(r'^[A-Za-z0-9]+$', longstring):
+        retval = longstring
+    else:
+        cleanstr = re.sub('[^A-Za-z0-9]+', '_', longstring)
+        cleanstr = re.sub('WRITE_IN_', 'wi_', cleanstr)
+        retval = cleanstr[:max_length]
+    return retval
+
+def _cand_tok_generation(targ, candblob):
+    i = next(i for i, cand in enumerate(candblob["List"]) if cand["Id"] == targ)
+    name = candblob['List'][i]['Description']
+    tok = _short_token(name)
+    return tok
+
+def _candidate_section_for_jabmod(contestid, candblob):
+    retval = {}
+    for c in candblob['List']:
+        if c["ContestId"] == contestid:
+            candtok = _short_token(c['Description'])
+            retval[candtok] = c['Description']
+
+    return retval
+
+def list_contests(container_path):
+    """Lists the contests in a San Francisco JSON CVR zip file."""
+    with zipfile.ZipFile(container_path, 'r') as zf:
+        with zf.open('ContestManifest.json') as f:
+            contestmanblob = json.load(f)
+
+        for contest in contestmanblob['List']:
+            print(f"Contest ID: {contest['Id']}, Description: {contest['Description']}")
+
+def convert_sfjson_to_jabmod(container_path, contestid=None):
+    """Converts a zip file of San Francisco JSON CVRs to a jabmod."""
+    abifmodel = get_emptyish_abifmodel()
+
+    with zipfile.ZipFile(container_path, 'r') as zf:
+        with zf.open('CandidateManifest.json') as f:
+            candblob = json.load(f)
+        with zf.open('ContestManifest.json') as f:
+            contestmanblob = json.load(f)
+        with zf.open('ElectionEventManifest.json') as f:
+            eventmanblob = json.load(f)
+
+        abifmodel['metadata']['ballotcount'] = 0
+        abifmodel['metadata']['emptyballotcount'] = 0
+        eventdesc = eventmanblob['List'][0]['Description']
+
+        abifmodel['metadata']['contestid'] = contestid
+        def _contest_index_lookup(targ, cmb):
+            try:
+                return next((i for i, contest in enumerate(cmb["List"]) if contest["Id"] == targ))
+            except:
+                print(f"{targ=}")
+                sys.exit()
+        if contestid:
+            contestindex = _contest_index_lookup(contestid, contestmanblob)
+        else:
+            contestindex = 0
+            contestid = contestmanblob['List'][contestindex]['Id']
+
+        title = f"{contestmanblob['List'][contestindex]['Description']} ({eventdesc})"
+        abifmodel['metadata']['title'] = title
+
+        # Add the candidates section
+        abifmodel['candidates'] = _candidate_section_for_jabmod(contestid, candblob)
+
+        # Add the votelines section
+        abifmodel['votelines'] = []
+        for filename in zf.namelist():
+            if filename.startswith('CvrExport_') and filename.endswith('.json'):
+                with zf.open(filename) as f:
+                    jsoncvr_blob = json.load(f)
+                for sess in jsoncvr_blob['Sessions']:
+                    for card in sess['Original']['Cards']:
+                        # Check if the card has the target contest
+                        has_target_contest = False
+                        for contest in card['Contests']:
+                            if contest['Id'] == contestid:
+                                has_target_contest = True
+                                break
+
+                        if has_target_contest:
+                            i = len(abifmodel['votelines'])
+                            abifmodel['metadata']['ballotcount'] += 1
+                            abifmodel['votelines'].append({})
+                            abifmodel['votelines'][i]['prefs'] = {}
+                            abifmodel['votelines'][i]['qty'] = 1
+                            for contest in card['Contests']:
+                                if contest['Id'] == contestid:
+                                    for m in contest['Marks']:
+                                        candtok = _cand_tok_generation(m['CandidateId'], candblob)
+                                        abifmodel['votelines'][i]['prefs'][candtok] = {}
+                                        abifmodel['votelines'][i]['prefs'][candtok]['rank'] = m['Rank']
+                            if abifmodel['votelines'][i]['prefs'] == {}:
+                                abifmodel['metadata']['emptyballotcount'] += 1
+
+    return abifmodel

pytests/json_test.py
@@ -266,6 +266,93 @@ testlist = [
                  ['metadata', 'ballotcount'],
                  1,
                  id='json_027'),
+    # TEST 028:
+    # Test parsing of the Tennessee example in SF CVR format
+    pytest.param(
+        ['-f', 'sfjson',
+         '--container', 'testdata/mock-elections/tennessee-example-sfjson-cvr.zip',
+         '--contestid', '1',
+         '-t', 'jabmod'
+        ],
+        None,
+        'is_equal',
+        ["metadata", "ballotcount"],
+        100,
+        id='json_028'
+    ),
+
+    # TEST 029:
+    # Test parsing of the Tennessee example in SF CVR format - specific voteline rank
+    pytest.param(
+        ['-f', 'sfjson',
+         '--container', 'testdata/mock-elections/tennessee-example-sfjson-cvr.zip',
+         '--contestid', '1',
+         '-t', 'jabmod'
+        ],
+        None,
+        'is_equal',
+        ["votelines", 0, "prefs", "Memph", "rank"],
+        1,
+        id='json_029'
+    ),
+    # TEST 030:
+    # Make sure that we have 100 ballots on race #2 of the sample zipfile
+    pytest.param(
+        ['-f', 'sfjson',
+         '--container', 'testdata/mock-elections/tennessee-example-sfjson-cvr.zip',
+         '--contestid', '2',
+         '-t', 'jabmod'
+        ],
+        None,
+        'is_equal',
+        ["metadata", "ballotcount"],
+        100,
+        id='json_030'
+    ),
+    # TEST 031:
+    # Make sure that Jackson shows up in race #2 in the sample zipfile
+    pytest.param(
+        ['-f', 'sfjson',
+         '--container', 'testdata/mock-elections/tennessee-example-sfjson-cvr.zip',
+         '--contestid', '2',
+         '-t', 'jabmod'
+        ],
+        None,
+        'is_equal',
+        ["votelines", 0, "prefs", "Jackson", "rank"],
+        1,
+        id='json_031'
+    ),
+    # TEST 032:
+    # Make sure Memph has 42 first-place votes in race #1 in the sample zipfile
+    pytest.param(
+        ['-f', 'sfjson',
+         '--container', 'testdata/mock-elections/tennessee-example-sfjson-cvr.zip',
+         '--contestid', '1',
+         '-t', 'jabmod',
+         '-m', 'consolidate'
+        ],
+        None,
+        'is_equal',
+        ["votelines", 0, "qty"],
+        42,
+        id='json_032'
+    ),
+    # TEST 033:
+    # Ensure Murfreesboro has 26 first-place votes in race #1 in the sample zipfile
+    pytest.param(
+        ['-f', 'sfjson',
+         '--container', 'testdata/mock-elections/tennessee-example-sfjson-cvr.zip',
+         '--contestid', '2',
+         '-t', 'jabmod',
+         '-m', 'consolidate'
+        ],
+        None,
+        'is_equal',
+        ["votelines", 1, "qty"],
+        26,
+        id='json_033'
+    )
 ]
 
 @pytest.mark.parametrize(
@@ -273,20 +360,16 @@ testlist = [
 )
 def test_json_key_subkey_val(cmd_args, inputfile, testtype, keylist, value):
     """Test equality of subkey to a value"""
-    # TODO: work out what I had planned with commit 56c3432e on
-    # 2023-10-08, since I'd like to use a generalized approach to
-    # skipping tests based on files that haven't been fetched with
-    # fetchmgr.py
-    try:
-        fh = open(inputfile, 'rb')
-        fh.close()
-    except:
-        msg = f'Missing file: {inputfile}'
-        msg += "Please run './fetchmgr.py *.fetchspec.json' "
-        msg += "if you haven't already"
-        pytest.skip(msg)
-
-    cmd_args.append(inputfile)
+    if inputfile:
+        try:
+            fh = open(inputfile, 'rb')
+            fh.close()
+        except:
+            msg = f'Missing file: {inputfile}'
+            msg += "Please run './fetchmgr.py *.fetchspec.json' "
+            msg += "if you haven't already"
+            pytest.skip(msg)
+        cmd_args.append(inputfile)
     abiftool_output = get_abiftool_output_as_array(cmd_args)
     outputdict = json.loads("\n".join(abiftool_output))
 

testdata/mock-elections/tennessee-vice-capital.abif
@@ -1,0 +1,14 @@
+{"title": "Tennessee 'Vice Capital' election"}
+{"description": "Same voters as the Tennessee capital example, but selecting a 'Vice Capital'. Preferences shift for regional balance. Uses Jackson (West), Murfreesboro (Central), Johnson City (East), and Kingsport (East)."}
+{"min_rating": 0}
+{"max_rating": 5}
+# -------------------------
+=Jackson:[Jackson]
+=Murfreesboro:[Murfreesboro]
+=Johnson_City:[Johnson City]
+=Kingsport:[Kingsport]
+# -------------------------
+42:Jackson/5>Murfreesboro/3>Johnson_City/1>Kingsport/0
+26:Murfreesboro/5>Jackson/3>Johnson_City/2>Kingsport/0
+15:Johnson_City/5>Kingsport/3>Murfreesboro/2>Jackson/0
+17:Kingsport/5>Johnson_City/4>Murfreesboro/2>Jackson/0

commit 83b5179b8c66f1074364bfc6c4b965d05f06440e

Fix(cli): Fix cli when no parameters given (+test)
This regression was introduced in 25083b4 when adding the --container
option (for archives). This is the first time it was valid NOT to have
an inputfile. This bug made it clear there weren't any tests for basic
CLI functionality, so there's now at least "cli_test.py" which
currently tests for no parameters, and is a good home for other CLI
tests in the future.

abiftool.py
@@ -141,13 +141,15 @@ def main():
     args = parser.parse_args()
     abiflib_test_log(f"cmd: {' '.join(sys.argv)}")
 
-    if args.list_contests:
-        if args.container:
-            list_contests(args.container)
-            sys.exit()
-        else:
-            print("Error: The --list-contests flag requires a container specified with --container.")
-            sys.exit()
+    if not args.input_file and not args.list_contests and not args.container:
+        parser.error("Missing input file.  Please specify an input file or"
+                     "--container for archive files.")
+    elif args.list_contests and args.container:
+        list_contests(args.container)
+        sys.exit()
+    elif args.list_contests and not args.container:
+        print("Error: The --list-contests flag requires a container specified with --container.")
+        sys.exit()
 
     # Determine input format based on file extension or override from
     # the "-f/--fromfmt" option

pytests/cli_test.py
@@ -1,0 +1,43 @@
+import pytest
+import subprocess
+import os
+from pytests.abiftestfuncs import get_abiftool_scriptloc
+
+# TODO: This function largely duplicates get_abiftool_output_as_array in abiftestfuncs.py.
+# It should be merged into abiftestfuncs.py to avoid duplication and provide a more
+# comprehensive utility for CLI testing (capturing stdout, stderr, and returncode).
+def get_abiftool_cli_output(cmd_args):
+    """
+    Runs abiftool.py with the given command-line arguments and captures its stdout, stderr, and return code.
+    """
+    command = [get_abiftool_scriptloc(), *cmd_args]
+    completed_process = subprocess.run(command,
+                                       stdout=subprocess.PIPE,
+                                       stderr=subprocess.PIPE,
+                                       text=True,
+                                       check=False) # Do not raise CalledProcessError for non-zero exit codes
+    return {
+        "stdout": completed_process.stdout,
+        "stderr": completed_process.stderr,
+        "returncode": completed_process.returncode
+    }
+
+test_cases = [
+    # No parameters
+    pytest.param([], "Missing input file.", "usage:", 2,
+                 id="cli_no_params_error"),
+]
+
+@pytest.mark.parametrize(
+    "cmd_args, expected_error_part, expected_usage_part, expected_returncode",
+    test_cases
+)
+def test_cli_no_params_error(cmd_args, expected_error_part, expected_usage_part, expected_returncode):
+    """
+    Test abiftool.py when run with no parameters, expecting a usage error.
+    """
+    result = get_abiftool_cli_output(cmd_args)
+
+    assert result["returncode"] == expected_returncode
+    assert expected_error_part in result["stderr"]
+    assert expected_usage_part in result["stderr"]